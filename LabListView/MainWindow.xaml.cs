bdf95530971700e634c029c840db031a642a3c22
Timothy Negron <37198273+negrt@users.noreply.github.com>
2019-08-11 18:51:56 -0400

    Add files via upload

---
+++ LabListView/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<VideoGame> VideoGameCollection = new ObservableCollection<VideoGame>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            VideoGameCollection.Add(new VideoGame { GameName = TextBoxGameName.Text, Rating = TextBoxRating.Text, Price = Convert.ToDouble(TextBoxPrice.Text),
+                                    Genre = TextBoxGenre.Text, Console = TextBoxConsole.Text });
+            ListViewVideoGames.ItemsSource = VideoGameCollection;
+
+            ClearTextBoxes();
+
+            TextBoxGameName.Focus();
+
+            TextBoxNumOfRecords.Text = Convert.ToString(VideoGameCollection.Count());
+        }
+
+        private void ClearTextBoxes()
+        {
+            TextBoxGameName.Clear();
+            TextBoxRating.Clear();
+            TextBoxPrice.Clear();
+            TextBoxGenre.Clear();
+            TextBoxConsole.Clear();
+        }
+    }
+}
---
+++ LabListView/VideoGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabListView
+{
+    class VideoGame
+    {
+        public string GameName { get; set; }
+        public string Rating { get; set; }
+        public double Price { get; set; }
+        public string Genre { get; set; }
+        public string Console { get; set; }
+    }
+}
